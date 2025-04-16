312ef000c8f8ff6653c8d249a98bb9b23526b9dc
AFKNoMercy <caotienminh2006@gmail.com>
2025-04-16 08:40:28 +0700

    update

--- file/Bài 3/3.c
+++ file/Bài 3/3.c
@@ -22,29 +22,21 @@ typedef struct
     Node *last;
 } Linkedlist;
 
-Linkedlist* create_list()
+Linkedlist *create_list()
 {
     Linkedlist *list = (Linkedlist *)malloc(sizeof(Linkedlist));
-    if (list == NULL)
-    {
-        perror("Không thể cấp phát bộ nhớ");
-        exit(EXIT_FAILURE);
-    }
     list->first = NULL;
     list->last = NULL;
     return list;
 }
 
-Node* create_node(HangHoa data)
+Node *create_node(HangHoa data)
 {
     Node *node = (Node *)malloc(sizeof(Node));
-    if (node == NULL)
-    {
-        perror("Không thể cấp phát bộ nhớ cho Node");
-        exit(EXIT_FAILURE);
-    }
+
     node->data = data;
     node->next = NULL;
+
     return node;
 }
 
@@ -102,10 +94,10 @@ void print_list(Linkedlist *list)
 {
     int i = 1, total = 0;
     Node *node = list->first;
-    printf("%5s %10s %10s %10s  %10s\n", "STT", "Ten Hang", "Don gia", "So luong", "Thanh tien");
+    printf("%5s %10s %10s %10s %10s\n", "STT", "Ten Hang", "Don gia", "So luong", "Thanh tien");
     while (node != NULL)
     {
-        printf("%5d %10s %10d %-10d  %10d\n", i++, node->data.TenHang, node->data.DonGia, node->data.SoLuong, node->data.ThanhTien);
+        printf("%5d %10s %10d %10d %10d\n", i++, node->data.TenHang, node->data.DonGia, node->data.SoLuong, node->data.ThanhTien);
         total += node->data.ThanhTien;
         node = node->next;
     }
@@ -117,7 +109,7 @@ void write_binary_file(Linkedlist *list, const char *filename)
     FILE *output = fopen(filename, "wb");
     if (output == NULL)
     {
-        perror("Khong the mo file");
+        perror("Khong the mo file\n");
         return;
     }
 
@@ -126,7 +118,7 @@ void write_binary_file(Linkedlist *list, const char *filename)
     {
         if (fwrite(&(node->data), sizeof(HangHoa), 1, output) != 1)
         {
-            perror("Loi ghi du lieu");
+            perror("Loi ghi du lieu\n");
             fclose(output);
             return;
         }
@@ -134,7 +126,54 @@ void write_binary_file(Linkedlist *list, const char *filename)
     }
 
     fclose(output);
-    printf("Da ghi vao file nhi phan");
+    printf("Da ghi vao file nhi phan\n");
+}
+
+void write_text_file(Linkedlist *list, const char *filename)
+{
+    FILE *output = fopen(filename, "w");
+    if (output == NULL)
+    {
+        perror("Khong the mo file\n");
+        return;
+    }
+    int i = 1, total = 0;
+    Node *node = list->first;
+    fprintf(output, "%5s %10s %10s %10s %10s\n", "STT", "Ten Hang", "Don gia", "So luong", "Thanh tien");
+    while (node != NULL)
+    {
+        fprintf(output, "%5d %10s %10d %10d %10d\n", i++, node->data.TenHang, node->data.DonGia, node->data.SoLuong, node->data.ThanhTien);
+        total += node->data.ThanhTien;
+        node = node->next;
+    }
+    fprintf(output, "%39s %10d\n", "Tong tien", total);
+
+    fclose(output);
+    printf("Da ghi vao file txt thanh cong\n");
+}
+
+void read_binary_file(const char *filename)
+{
+    FILE *input = fopen(filename, "rb");
+    if (input == NULL)
+    {
+        perror("Không thể mở file");
+        return;
+    }
+
+    HangHoa hang;
+    int i = 1, total = 0;
+
+    printf("%5s %10s %10s %10s %10s\n", "STT", "Tên Hàng", "Đơn Giá", "Số Lượng", "Thành Tiền");
+    while (fread(&hang, sizeof(HangHoa), 1, input) == 1)
+    {
+        printf("%5d %10s %10d %10d %10d\n", i++, hang.TenHang, hang.DonGia, hang.SoLuong, hang.ThanhTien);
+        total += hang.ThanhTien;
+    }
+    printf("%39s %10d\n", "Tổng Tiền", total);
+
+    fclose(input);
+    printf("Đã đọc file nhị phân thành công.\n");
 }
 
 int main()
@@ -153,5 +192,6 @@ int main()
 
     write_binary_file(list, "output_3.bin");
 
+    write_text_file(list, "output_3.txt");
     return 0;
 }
